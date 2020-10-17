e7dbdb047c93924da07c28b467ebd7fc6e140ae4
anatcruz <anateresacruz2000@gmail.com>
2020-10-17 17:14:44 +0100

    Started ll functions

---
+++ TP1/src/ll.c
@@ -0,0 +1,98 @@
+#include "ll.h"
+
+linkLayer linklayer;
+
+int llopen(char* port, int flag){
+    int fd, res;
+    strcpy(linklayer.port, port);
+
+    fd = openSerial();
+    
+    if(flag == RECEIVER){ //RECEIVER
+        unsigned char check[2];
+	    unsigned char byte;
+	    enum states state = START;
+	
+	    while (state != STOP) {       /* loop for input */
+		if (read(fd,&byte,1) == -1)   /* returns after 1 char has been input */
+			printf("Error reading SET byte\n");
+		else
+			processFrameSU(&state, byte);
+	    }
+
+	    printf("Recived SET\n");
+	
+        if ((res=sendUA(fd)) == -1)
+            printf("Error sending UA\n");
+        else
+            printf("%d UA bytes written\n", res);
+    }
+
+    else if(flag == TRANSMITTER){
+        unsigned char byte;
+        
+        do {
+            if ((res=sendSET(fd)) == -1)
+                printf("Error sending SET\n");
+            else
+                printf("%d SET bytes written\n", res);
+
+            alarm(3);
+            linklayer.alarm=0;
+
+            enum states state = START;
+            unsigned char check[2];
+            while(state!=STOP && !linklayer.alarm) {
+                if (read(fd, &byte, 1) == -1)
+                    printf("Error reading UA byte\n");
+                else
+				    processFrameSU(&state, byte);
+            }
+            if (linklayer.alarm) printf("Timed out! Retrying\n");
+        } while(linklayer.numTransmissions < MAX_TRANSMISSIONS && linklayer.alarm);
+    }
+
+    return fd; 
+}
+
+int openSerial(){
+    /*
+	Open serial port device for reading and writing and not as controlling tty
+	because we don't want to get killed if linenoise sends CTRL-C.
+    */
+    int fd = open(linklayer.port, O_RDWR | O_NOCTTY);
+	if (fd <0) {perror(linklayer.port); exit(-1); }
+
+	if ( tcgetattr(fd,&linklayer.oldtio == -1) ){ /* save current port settings */
+		perror("tcgetattr");
+		exit(-1);
+	}
+
+	bzero(&linklayer.newtio, sizeof(linklayer.newtio));
+	linklayer.newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+	linklayer.newtio.c_iflag = IGNPAR;
+	linklayer.newtio.c_oflag = 0;
+
+	/* set input mode (non-canonical, no echo,...) */
+	linklayer.newtio.c_lflag = 0;
+
+	linklayer.newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+	linklayer.newtio.c_cc[VMIN]     = 1;   /* blocking read until 1 chars received */
+
+    /* 
+	VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
+	leitura do(s) pr�ximo(s) caracter(es)
+    */
+
+	tcflush(fd, TCIOFLUSH);
+
+	if ( tcsetattr(fd,TCSANOW,&linklayer.newtio) == -1) {
+		perror("tcsetattr");
+		exit(-1);
+	}
+
+	printf("New termios structure set\n");
+    return fd;
+
+}
+
---
+++ TP1/src/ll.h
@@ -0,0 +1,44 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "messages.h"
+
+#define BAUDRATE B38400
+#define MODEMDEVICE "/dev/ttyS1"
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define TRANSMITTER 0
+#define RECEIVER 1
+#define MAX_TRANSMISSIONS 3
+
+typedef struct 
+{
+    int fileDescriptor;
+    int status; //TRANSMITTER/RECEIVER
+} applicationLayer;
+
+typedef struct 
+{
+    char port[20]; // /dev/ttySx
+    int baudRate;  //transmission speed
+    unsigned int sequenceNumber; //trama sequence
+    unsigned int timeout;
+    unsigned int numTransmissions; //atempt number in case of failure
+    unsigned int alarm;
+    char frame[256]; //trama
+    struct termios oldtio, newtio;
+} linkLayer;
+
+extern linkLayer linklayer;
+
+//ll functions
+int llopen(char* port, int flag);
+int llclose(int fd);
+
+
+int openSerial();
