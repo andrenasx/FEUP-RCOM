db17cbc7c3e9c93f90733974384fbc3c5613afae
andrenasx <andre2000.an@gmail.com>
2020-11-08 15:55:06 +0000

    15% BCC2

--- TP1/src/messages.c
+++ TP1/src/messages.c
@@ -209,7 +209,7 @@ int readFrameI(int fd, unsigned char *frame){
         frame[length++] = byte;
     }
 
-    generateBCC2Error(frame, 10);
+    generateBCC2Error(frame, 15);
 
     linklayer.stats.numReceivedFramesI++;
     return length;
