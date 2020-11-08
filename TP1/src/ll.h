907e065ffc65221d039c08cee5e6397f879e554a
andrenasx <andre2000.an@gmail.com>
2020-11-08 12:04:03 +0000

    C = 9600

--- TP1/src/ll.h
+++ TP1/src/ll.h
@@ -11,7 +11,7 @@
 #include "messages.h"
 #include "alarm.h"
 
-#define BAUDRATE B4800
+#define BAUDRATE B9600
 #define MODEMDEVICE "/dev/ttyS1"
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define TRANSMITTER 0
