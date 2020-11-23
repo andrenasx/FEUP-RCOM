331d98c3d5866d786e44bc03c4fd84a84a46c3ee
anatcruz <anateresacruz2000@gmail.com>
2020-11-23 15:38:24 +0000

    Started socket handler

---
+++ TP2/src/socket_handler.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <netdb.h>
+#include <strings.h>
+
+int open_socket(const char* ip_address, const int port);
